f4e861a6c417d85807e6b5d3c4f364bb145b3a87
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:29:52 +0530

    DataBase

--- JDBC/src/Read.java
+++ JDBC/src/Read.java
@@ -6,6 +6,9 @@ class Displaying{
     private static final String url="jdbc:mysql://localhost:3306/my_DB";
     private static final String username="root";
     private static final String password="@Radhakrishna297";
+    public void disp(){
+        
+    }
 }
 public class Read {
     
