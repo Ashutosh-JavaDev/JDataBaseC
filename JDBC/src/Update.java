e8c038fd149d398876022a92898b5337c1a0f9e2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:17:20 +0530

    DataBase

--- JDBC/src/Update.java
+++ JDBC/src/Update.java
@@ -7,6 +7,15 @@ class Updating{
     private static final String url="jdbc:mysql://localhost:3306/my_DB";
     private static final String username="root";
     private static final String password="@Radhakrishna297"; 
+    public void UpdatingData(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+            return;
+        }
+    }
 }
 public class Update {
     
