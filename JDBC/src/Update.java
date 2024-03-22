2e0b89dddb9bd1d6d75ea2b28de7837bfd809b2c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:22:52 +0530

    DataBase

--- JDBC/src/Update.java
+++ JDBC/src/Update.java
@@ -19,6 +19,13 @@ class Updating{
             Connection connection=DriverManager.getConnection(url, username, password);
             Statement statement=connection.createStatement();
             String query=String.format("Update Employee set username =Sanatani where id=3");
+            int Affectedrow=statement.executeUpdate(query);
+            if(Affectedrow>0){
+                System.out.println("Data Updated Successfully!");
+            }
+            else{
+                System.out.println("Data Not Updated!");
+            }
         }
         catch(SQLException e){
             e.printStackTrace();
