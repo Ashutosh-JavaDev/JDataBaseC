4b4088f9914e0b93be6f0ea496f320be06216794
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:14:54 +0530

    DataBase

--- JDBC/src/Update.java
+++ JDBC/src/Update.java
@@ -5,6 +5,8 @@ import java.sql.Statement;
 import java.sql.SQLException;
 class Updating{
     private static final String url="jdbc:mysql://localhost:3306/my_DB";
+    private static final String username="root";
+    private static final String password="@Radhakrishna297"; 
 }
 public class Update {
     
