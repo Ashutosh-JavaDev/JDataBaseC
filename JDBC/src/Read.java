243b15d6be7db2d89c9dd4bda6a02cac4eaa002b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:28:58 +0530

    DataBase

---
+++ JDBC/src/Read.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+class Displaying{
+    private static final String url="jdbc:mysql://localhost:3306/my_DB";
+    private static final String username="root";
+    private static final String password="@Radhakrishna297";
+}
+public class Read {
+    
+}
