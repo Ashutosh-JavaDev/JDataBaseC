5060ac8240eca0b238633c2578dd6709dffd2c95
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 22:02:14 +0530

    DataBase

---
+++ JDBC/src/Update.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+class Updating{
+    
+}
+public class Update {
+    
+}
