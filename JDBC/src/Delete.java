b0f2d3642a882d5ac0c4812cfec2289d521e3b49
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 21:01:39 +0530

    DataBase

---
+++ JDBC/src/Delete.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+class Deleting{
+    private static final String url="jdbc:mysql://localhost:3306/my_DB";
+    private static final String username="root";
+    private  static final String password="@Radhakrishna297";
+}
+public class Delete {
+    
+}
--- JDBC/src/Insert.java
+++ JDBC/src/Insert.java
@@ -3,7 +3,7 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 class DBase{
-    private static final String url="jdbc:mysql://localhost:3306/?my_DB";
+    private static final String url="jdbc:mysql://localhost:3306/my_DB";
     private static final String username="root";
     private  static final String password="@Radhakrishna297";
     public void data(){
