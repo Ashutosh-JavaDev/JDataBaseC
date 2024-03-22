57dafba8bce081622ccd450927af0f30f76c3092
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 20:50:32 +0530

    DataBase

--- JDBC/src/Insert.java
+++ JDBC/src/Insert.java
@@ -1,17 +1,18 @@
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
-import java.sql.SQLDataException;
+import java.sql.SQLException;
 class DBase{
     private static final String url="jdbc:mysql://localhost:3306/?my_DB";
     private static final String username="root";
     private  static final String password="@Radhakrishna297";
-    public static void data(){
+    public void data(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch(ClassNotFoundException e){
             e.printStackTrace();
+            return;
         }
         try{
             Connection connection=DriverManager.getConnection(url, username, password);
@@ -25,7 +26,7 @@ class DBase{
                 System.out.println("Data NNot Inserted");
             }
         }
-        catch(Exception e){
+        catch(SQLException e){
             e.printStackTrace();
         }
     }
