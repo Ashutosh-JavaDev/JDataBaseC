bf8199d4c0ffe0b37b74d05c946053ffd327cdd0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-22 20:16:17 +0530

    DataBase

---
+++ JDBC/src/Insert.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+class DBase{
+    private static final String url="jdbc:mysql://localhost:3306/?user=root";
+    private static final String username="root";
+    private  static final String password="@Radhakrishna297";
+    public static void data(){
+
+    }
+}
+public class Insert {
+    
+}
