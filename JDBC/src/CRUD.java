2286cbbc2a9c15df2010c928d569ccf9a14c84ad
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-25 13:26:03 +0530

    Database

--- JDBC/src/CRUD.java
+++ JDBC/src/CRUD.java
@@ -19,7 +19,13 @@ class crud {
             Statement statement = connection.createStatement();
             String query = "Insert into Employee(id,name,Degination)values" + ("4,Aman,IT") + ("5,Ritika,Web Designer")
                     + ("6,Akash,UI/UX") + ("7,Prachi,Software Developer");
-
+            int result=statement.executeUpdate(query);
+            if(result>0){
+                System.out.println("Data Insert SuccessFully");
+            }
+            else{
+                System.out.println("Data Not Inserted");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
