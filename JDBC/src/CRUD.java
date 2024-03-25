122ed9add4577f5bcf70ea2d21956db1504de6a8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-25 15:15:37 +0530

    Project

--- JDBC/src/CRUD.java
+++ JDBC/src/CRUD.java
@@ -3,6 +3,10 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.ResultSet;
+import javax.naming.spi.DirStateFactory.Result;
+
+import com.mysql.cj.jdbc.Driver;
 
 class crud {
     private static final String url = "jdbc:mysql://localhost:3306/my_DB";
@@ -10,44 +14,68 @@ class crud {
     private static final String password = "@Radhakrishna297";
 
     // public void Inserting() {
-    //     try {
-    //         Class.forName("com.mysql.cj.jdbc.Driver");
-    //     } catch (ClassNotFoundException e) {
-    //         e.printStackTrace();
-    //     }
-    //     try {
-    //         Connection connection = DriverManager.getConnection(url, username, password);
-    //         Statement statement = connection.createStatement();
-    //         Scanner scanner = new Scanner(System.in);
-
-    //         while (true) {
-    //             System.out.println("Enter employee ID (or type 'stop' to stop): ");
-    //             String idInput = scanner.nextLine().trim();
-    //             if (idInput.equalsIgnoreCase("stop")) {
-    //                 break;
-    //             }
-    //             int id = Integer.parseInt(idInput);
-
-    //             System.out.print("Enter employee name: ");
-    //             String name = scanner.nextLine();
-
-    //             System.out.print("Enter employee designation: ");
-    //             String designation = scanner.nextLine();
-
-    //             String query = String.format("Insert into Employee(id,name,Degination) values (%d,'%s','%s')", id, name,
-    //                     designation);
-    //             int result = statement.executeUpdate(query);
-    //             if (result > 0) {
-    //                 System.out.println("Data Inserted Successfully");
-    //             } else {
-    //                 System.out.println("Data Not Inserted");
-    //             }
-    //         }
-
-    //     } catch (SQLException e) {
-    //         e.printStackTrace();
-    //     }
+    // try {
+    // Class.forName("com.mysql.cj.jdbc.Driver");
+    // } catch (ClassNotFoundException e) {
+    // e.printStackTrace();
     // }
+    // try {
+    // Connection connection = DriverManager.getConnection(url, username, password);
+    // Statement statement = connection.createStatement();
+    // Scanner scanner = new Scanner(System.in);
+
+    // while (true) {
+    // System.out.println("Enter employee ID (or type 'stop' to stop): ");
+    // String idInput = scanner.nextLine().trim();
+    // if (idInput.equalsIgnoreCase("stop")) {
+    // break;
+    // }
+    // int id = Integer.parseInt(idInput);
+
+    // System.out.print("Enter employee name: ");
+    // String name = scanner.nextLine();
+
+    // System.out.print("Enter employee designation: ");
+    // String designation = scanner.nextLine();
+
+    // String query = String.format("Insert into Employee(id,name,Degination) values
+    // (%d,'%s','%s')", id, name,
+    // designation);
+    // int result = statement.executeUpdate(query);
+    // if (result > 0) {
+    // System.out.println("Data Inserted Successfully");
+    // } else {
+    // System.out.println("Data Not Inserted");
+    // }
+    // }
+
+    // } catch (SQLException e) {
+    // e.printStackTrace();
+    // }
+    // }
+    public void excep() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try{
+            Connection conn=DriverManager.getConnection(url, username, password);
+            Statement statement=conn.createStatement();
+            String queue="select*from Employee";
+            ResultSet res=statement.executeQuery(queue);
+            while(res.next()){
+                int id=res.getInt("id");
+                String name=res.getString("name");
+                String Desi=res.getString("Degination");
+                System.out.print("|ID: "+id+" |Name: "+name+" |Degination: "+Deg+"|");
+                System.out.println();
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 
     public void Updateing() {
         Scanner sc = new Scanner(System.in);
@@ -67,7 +95,7 @@ class crud {
                     int oldId = sc.nextInt();
                     System.out.print("New ID Number: ");
                     int newID = sc.nextInt();
-                    String query = String.format("Update Employee set id =%o where id=%o",newID,oldId);
+                    String query = String.format("Update Employee set id =%o where id=%o", newID, oldId);
                     int result = statement.executeUpdate(query);
                     if (result > 0) {
                         System.out.println("Data Updated Succesfully");
@@ -81,7 +109,7 @@ class crud {
                     sc.nextLine();
                     System.out.print("New Name: ");
                     String newName = sc.nextLine();
-                    String level = String.format("Update Employee set name ='%s' where id=%o",newName,uID);
+                    String level = String.format("Update Employee set name ='%s' where id=%o", newName, uID);
                     int affectedRow = statement.executeUpdate(level);
                     if (affectedRow > 0) {
                         System.out.println("Data Updated Succesfully");
@@ -96,7 +124,8 @@ class crud {
 
                     System.out.print("New Degination: ");
                     String newDesignation = sc.nextLine();
-                    String low = String.format("Update Employee set Degination ='%s' where id=%o",newDesignation,usID);
+                    String low = String.format("Update Employee set Degination ='%s' where id=%o", newDesignation,
+                            usID);
                     int Affected = statement.executeUpdate(low);
                     if (Affected > 0) {
                         System.out.println("Data Updated Succesfully");
