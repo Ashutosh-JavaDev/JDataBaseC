c66ba4be5da966b901e42efe6b14982d81918376
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-25 13:55:10 +0530

    Project

--- JDBC/src/CRUD.java
+++ JDBC/src/CRUD.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 class crud {
     private static final String url = "jdbc:mysql://localhost:3306/my_DB";
@@ -10,22 +11,39 @@ class crud {
 
     public void Inserting() {
         try {
-            Class.forName("com.mysql.cj.DriveManger");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
-            String query = "Insert into Employee(id,name,Degination)values" + ("4,Aman,IT") + ("5,Ritika,Web Designer")
-                    + ("6,Akash,UI/UX") + ("7,Prachi,Software Developer");
-            int result=statement.executeUpdate(query);
-            if(result>0){
-                System.out.println("Data Insert SuccessFully");
+            Scanner scanner = new Scanner(System.in);
+
+           
+            while (true) {
+                System.out.println("Enter employee ID (or type 'stop' to stop): ");
+                String idInput = scanner.nextLine().trim();
+                if (idInput.equalsIgnoreCase("stop")) {
+                    break;
+                }
+                int id = Integer.parseInt(idInput);
+
+                System.out.print("Enter employee name: ");
+                String name = scanner.nextLine();
+
+                System.out.print("Enter employee designation: ");
+                String designation = scanner.nextLine();
+
+                String query = String.format("Insert into Employee(id,name,Degination) values (%d,'%s','%s')", id, name, designation);
+                int result = statement.executeUpdate(query);
+                if (result > 0) {
+                    System.out.println("Data Inserted Successfully");
+                } else {
+                    System.out.println("Data Not Inserted");
+                }
             }
-            else{
-                System.out.println("Data Not Inserted");
-            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -33,5 +51,8 @@ class crud {
 }
 
 public class CRUD {
-
+    public static void main(String[] args) {
+        crud ob = new crud();
+        ob.Inserting();
+    }
 }
