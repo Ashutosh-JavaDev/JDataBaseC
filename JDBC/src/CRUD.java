ede30094dfda8fa60ea7a9e0f6ead94176d43184
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-25 12:55:59 +0530

    Database

--- JDBC/src/CRUD.java
+++ JDBC/src/CRUD.java
@@ -3,7 +3,17 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 class crud{
-    
+    private static final String url="jdbc:mysql://localhost:3306/my_DB";
+    private static final String username="root";
+    private  static final String password="@Radhakrishna297";
+    public void curding(){
+        try{
+            Class.forName("com.mysql.cj.DriveManger");
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
 public class CRUD {
     
