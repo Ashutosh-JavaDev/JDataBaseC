d240604d594347f84522553f7158a5da55a28985
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-30 12:33:46 +0530

    Java Program

--- JDBC/src/CRUD.java
+++ JDBC/src/CRUD.java
@@ -65,7 +65,7 @@ class crud {
                 String Desi = scanner.nextLine();
                 System.out.print("Employee Status\n('A'Active/'IN'InActive/'L'Leave): ");
                 String Stat = scanner.nextLine().toUpperCase();
-                String query = String.format("Insert into Employee(id,name,Degination) values(%d,'%s','%s',yy-MM-dd,'%s','%s',%d,'%s','%s','%s','%s')", id, Fname,Lname);
+                String query = String.format("Insert into Employee(id,name,Degination) values(%d,'%s','%s',yy-MM-dd,'%s','%s',%d,'%s','%s','%s','%s')", id, Fname,Lname,dobInput,gender,email,phone,City,sal,Desi,Stat);
                 int result = statement.executeUpdate(query);
                 if (result > 0) {
                     System.out.println("Data Inserted Successfully");
